fa64379a1bbc4cfa262b3f56d1f3869cb5b12012
lefline <issamkharbache2@gmail.com>
2024-10-19 18:19:44 +0100

    deleting the user avatar from uploadthing after user changing his avatar

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createUploadthing, FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 const f = createUploadthing();
 
 export const fileRouter = {
@@ -11,11 +11,17 @@ export const fileRouter = {
     .middleware(async ({ req, res }) => {
       const { user } = await validateRequest();
       if (!user) throw new UploadThingError("Unauthorized");
-      console.log(res);
 
       return { user };
     })
     .onUploadComplete(async ({ metadata, file }) => {
+      const oldAvatar = metadata.user.avatarUrl;
+      if (oldAvatar) {
+        const key = oldAvatar.split(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+        )[1];
+        await new UTApi().deleteFiles([key]);
+      }
       const newAvatarUrl = file.url.replace(
         "/f/",
         `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,

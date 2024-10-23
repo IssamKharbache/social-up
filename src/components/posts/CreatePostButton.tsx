a75a1b385b7da89f29ecc0426a4e437262871857
lefline <issamkharbache2@gmail.com>
2024-10-23 18:59:07 +0100

    hide post button in mobile

--- src/components/posts/CreatePostButton.tsx
+++ src/components/posts/CreatePostButton.tsx
@@ -9,15 +9,18 @@ const CreatePostButton = () => {
   const { openModal, setOpenModal } = useUser();
   return (
     <>
-      <Button
-        onClick={() => setOpenModal(true)}
-        variant="default"
-        className="flex w-full items-center gap-3 rounded-full p-4 text-center text-lg"
-        title="Post"
-      >
-        <PlusIcon className="size-4 md:hidden" />
-        <span className="hidden md:block">Post</span>
-      </Button>
+      <div className="hidden md:block">
+        {" "}
+        <Button
+          onClick={() => setOpenModal(true)}
+          variant="default"
+          className="flex w-full items-center gap-3 rounded-full p-4 text-center text-lg"
+          title="Post"
+        >
+          <PlusIcon className="size-4 md:hidden" />
+          <span className="hidden md:block">Post</span>
+        </Button>
+      </div>
       {openModal && (
         <CreatePostModal open={openModal} onClose={() => setOpenModal(false)} />
       )}
